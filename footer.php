6724d8d8d5585c69ea0e4a22a929a6683edce11e
SerlyOktaviani19126 <72858044+SerlyOktaviani@users.noreply.github.com>
2021-04-24 23:56:58 +0700

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,8 @@
+<footer class="blog-footer py-4 mt-4">
+    <p>
+        Serly Oktaviani<br>
+        Student STT NF &copy; 2021
+    <p>
+        <a href="#" class="text-muted">Back to top</a>
+    </p>
+</footer>
---
+++ home.php
@@ -0,0 +1,4 @@
+<div class="jumbotron">
+    <h1 class="display-4 mb-4">Welcome to MyApp!</h1>
+    <p class="lead">Belajar CRUD</p>
+</div>
