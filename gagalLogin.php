aa4bc3cac5e2a4c19fd7f0225464984189fd4e0c
SerlyOktaviani19126 <72858044+SerlyOktaviani@users.noreply.github.com>
2021-04-25 09:05:53 +0700

    Update gagalLogin.php

--- gagalLogin.php
+++ gagalLogin.php
@@ -2,5 +2,5 @@
     <h1 class="display-4 mb-3">Gagal Login</h1>
     <p>Username atau password yang anda masukan salah! :(</p>
     <a class="btn btn-secondary" href="index.php?hal=formLogin" role="button">Back to login form</a>
-    <p class="text-monospace mt-4">Username kim<br>Password 123</p>
+    <p class="text-monospace mt-4">Username ser <br>Password 123</p>
 </div>
